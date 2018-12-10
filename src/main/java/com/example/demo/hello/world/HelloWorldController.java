cb0c2a62e7a9aef5413ef0b2f3b7ada4808bdc7a
Invitados <Invitados@POOLMED96655.pslcol.com.co>
2018-12-10 14:41:39 -0500

    add environment variable HELLO

--- src/main/java/com/example/demo/hello/world/HelloWorldController.java
+++ src/main/java/com/example/demo/hello/world/HelloWorldController.java
@@ -1,15 +1,18 @@
 package com.example.demo.hello.world;
 
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
+    @Value("${HELLO:Hola}")
+    private String greeting;
 
     @GetMapping("/hello")
     public String helloWorld(){
 
-        return "hello Icesi";
+        return greeting+ " Icesi";
     }
 }
