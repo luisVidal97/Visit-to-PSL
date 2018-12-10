4c2c42d774b10e7dc4458025c01d4653d06be218
Invitados <Invitados@POOLMED96655.pslcol.com.co>
2018-12-10 14:50:38 -0500

    add name

--- src/main/java/com/example/demo/hello/world/HelloWorldController.java
+++ src/main/java/com/example/demo/hello/world/HelloWorldController.java
@@ -3,6 +3,7 @@ package com.example.demo.hello.world;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,9 +11,9 @@ public class HelloWorldController {
     @Value("${HELLO:Hola}")
     private String greeting;
 
-    @GetMapping("/hello")
-    public String helloWorld(){
+    @GetMapping("/hello/{name}")
+    public String helloWorld(@PathVariable("name")String name){
 
-        return greeting+ " Icesi";
+        return greeting+ " "+name;
     }
 }
